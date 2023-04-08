4b466e2a49cc6352b881e7e2f192a4171197b78a
sylvain <sylvain@delmote.me>
2023-04-08 21:41:42 +0200

    Dark Mode swich

--- src/components/Global/DarkModeSwitch.vue
+++ src/components/Global/DarkModeSwitch.vue
@@ -1,6 +1,32 @@
 <script setup>
+import { useQuasar } from 'quasar'
+import { ref, watch } from 'vue'
+
+const $q = useQuasar()
+const pluginIsEnabled = Boolean($q.dark.mode)
+const isInDevMode = process.env.DEV
+const isDark = ref($q.dark.isActive)
+
+const toggleDarkMode = () => {
+  $q.dark.toggle()
+}
+
+watch(isDark, toggleDarkMode)
+
 </script>
 
 <template>
-  <q-btn color="primary" icon="check" label="OK" @click="onClick" />
+  <q-toggle
+  v-model="isDark"
+  color="accent"
+  :label= "`${pluginIsEnabled && isInDevMode ? ' ':'Activez le module Dark dans framework/config' }`"
+  left-label
+  checked-icon="dark_mode"
+  unchecked-icon="light_mode"
+  :disable = !pluginIsEnabled
+  >
+  <q-tooltip >
+    {{ isDark ? "Dark Mode" :" Light Mode"}}
+  </q-tooltip>
+  </q-toggle>
 </template>
